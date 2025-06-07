51719dbb04a75500412e07a7e0888a2ad6bc9eb1
Amir Ridwan <amirridwan.dev@gmail.com>
2025-06-07 23:59:16 +0800

    feat: Refactor App component to display player name and score with question answers

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { questionAnswers } from "./constants/questionAnswers";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [player, setPlayer] = useState({
+    name: "Amir",
+    score: 0,
+  });
+
+  const [nearestValue, setNearestValue] = useState(10);
+  const alphabetNumbering = ["a.", "b.", "c.", "d."];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-white h-screen w-screen">
+      {/*title*/}
+      <div>Rounding Off to Nearest 10</div>
+
+      {/*name and score*/}
+      <div className="flex flex-row gap-5 justify-center p-5">
+        <div>
+          <h2>Name</h2>
+          <h3>{player.name}</h3>
+        </div>
+        <div>
+          <h2>Score</h2>
+          <h3>{player.score}</h3>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div>Circle the correct answer</div>
+      {/*question*/}
+      <div className="grid grid-cols-2">
+        {questionAnswers.map((qA) => {
+          return (
+            <div key={qA.number} className="my-2 text-left mx-auto">
+              <div className="py-2">
+                {qA.number} rounded off to the nearest {nearestValue.toString()}{" "}
+                is
+              </div>
+              <ol className="text-left w-full mx-auto py-2">
+                {qA.answers.map((answer, index) => {
+                  return (
+                    <li key={`${index}-${answer}`} className="py-1 flex cursor-pointer w-max">
+                      {alphabetNumbering[index]} {answer}
+                    </li>
+                  );
+                })}
+              </ol>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/*copyright*/}
+      <div className="py-3">
+        © www.mathinenglish.com
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
