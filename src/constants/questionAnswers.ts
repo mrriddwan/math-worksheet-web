fb12a32b82b6405338abb9c8abc61e15e73a756d
Amir Ridwan <amirridwan.dev@gmail.com>
2025-06-07 23:58:31 +0800

    feat: Add questionAnswers constant with predefined values

---
+++ src/constants/questionAnswers.ts
@@ -0,0 +1,50 @@
+export const questionAnswers = [
+    {
+        number: 17,
+        answers: [10, 20, 17],
+    },
+    {
+        number: 75,
+        answers: [70, 80, 175],
+    },
+    {
+        number: 64,
+        answers: [64, 70, 70],
+    },
+    {
+        number: 98,
+        answers: [80, 100, 89],
+    },
+    {
+        number: 94,
+        answers: [100, 94, 90],
+    },
+    {
+        number: 445,
+        answers: [450, 440, 500],
+    },
+    {
+        number: 45,
+        answers: [50, 45, 40],
+    },
+    {
+        number: 19,
+        answers: [20, 10, 19],
+    },
+    {
+        number: 0,
+        answers: [10, 1, 0],
+    },
+    {
+        number: 199,
+        answers: [190, 100,200],
+    },
+    {
+        number: 165,
+        answers: [160, 170, 150],
+    },
+    {
+        number: 999,
+        answers: [990, 1000, 909],
+    },
+];
